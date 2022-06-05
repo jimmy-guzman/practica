a85455667dad5ea0121a931cfbef53c8c93f1771
jimmy-guzman <hi@jimmyguzman.com>
2022-06-05 13:42:23 -0500

    feat: ✨ add average salary problem

--- src/count-odd-numbers.ts
+++ src/count-odd-numbers.ts
@@ -6,22 +6,6 @@
  * @see https://leetcode.com/problems/count-odd-numbers-in-an-interval-range
  */
 const countOdds = (low: number, high: number): number => {
-  if (!Number.isInteger(low)) {
-    throw new Error('low must be an integer')
-  }
-
-  if (low < 0) {
-    throw new Error('low must be a non negative integer')
-  }
-
-  if (!Number.isInteger(high)) {
-    throw new Error('high must be an integer')
-  }
-
-  if (high < 0) {
-    throw new Error('high must be a non negative integer')
-  }
-
   return Math.floor((high - low) / 2) + (low & 1 || high & 1)
 }
 
@@ -38,28 +22,5 @@ if (import.meta.vitest) {
   })
   it('should return 1 when low is 21 and high is 22', () => {
     expect(countOdds(21, 22)).toBe(1)
-  })
-  it('should throw when low is not an integer', () => {
-    expect(countOdds(8, 10)).toBe(1)
-  })
-  it('should throw when low is not an integer', () => {
-    expect(() => {
-      countOdds(0.8, 10)
-    }).toThrowError(Error('low must be an integer'))
-  })
-  it('should throw when low is a negative number', () => {
-    expect(() => {
-      countOdds(-8, 10)
-    }).toThrowError(Error('low must be a non negative integer'))
-  })
-  it('should throw when high is not an integer', () => {
-    expect(() => {
-      countOdds(8, 0.1)
-    }).toThrowError(Error('high must be an integer'))
-  })
-  it('should throw when high is not an integer', () => {
-    expect(() => {
-      countOdds(8, -10)
-    }).toThrowError(Error('high must be a non negative integer'))
   })
 }
